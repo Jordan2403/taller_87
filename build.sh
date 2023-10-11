c2e3a27d7c6b0544d27b5a44e8c093ac9ac159ca
KjDm2020123 <estsjkjdm2004@gmail.com>
2023-10-11 17:04:12 -0500

    first commit

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+DJANGO_SUPERUSER_PASSWORD=taller87 python manage.py createsuperuser --no-input --username=kevin  --email=joe@example.com
